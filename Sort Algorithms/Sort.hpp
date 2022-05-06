3fb364f32a942a36e197c89fc320489ff6d51d58
Bryan <elton.flores.a@uni.pe>
2022-05-06 12:42:17 -0500

    adding some sort algorithms

---
+++ Sort Algorithms/Sort.hpp
@@ -0,0 +1,42 @@
+class Sort {
+public:
+  void swapSort(int *array, int size) {
+    int temp;
+    for (int i = 0; i < size - 1; i++) {
+      for (int j = i + 1; j < size; j++) {
+        if (array[i] > array[j]) {
+          temp = array[j];
+          array[j] = array[i];
+          array[i] = temp;
+        }
+      }
+    }
+  }
+  void selectionSort(int *array, int size) {
+    int temp;
+    for (int i = 0; i < size - 1; i++) {
+      int min = i;
+      for (int j = i + 1; j < size; j++) {
+        if (array[j] < array[min]) {
+          min = j;
+        }
+      }
+      temp = array[i];
+      array[i] = array[min];
+      array[min] = temp;
+    }
+  }
+  void insertionSort(int *array, int size) {
+    for (int i = 0; i < size; i++) {
+      int j = 0;
+      while (j < i) {
+        if (array[i] < array[j]) {
+          int temp = array[i];
+          array[i] = array[j];
+          array[j] = temp;
+        }
+        j++;
+      }
+    }
+  }
+};
---
+++ Sort Algorithms/main.cpp
@@ -0,0 +1,23 @@
+#include "Sort.hpp"
+#include <iostream>
+using namespace std;
+int *generateArray(int size) {
+  int *array = new int[size];
+  for (int i = 0; i < size; i++) {
+    array[i] = rand() % 100;
+  }
+  return array;
+}
+void printArray(int *array, int size) {
+  for (int i = 0; i < size; i++) {
+    cout << array[i] << " ";
+  }
+  cout << endl;
+}
+int main() {
+  int *array = generateArray(10);
+  printArray(array, 10);
+  Sort *sort = new Sort();
+  sort->insertionSort(array, 10);
+  printArray(array, 10);
+}
