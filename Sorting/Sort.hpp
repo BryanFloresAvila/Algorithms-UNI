a703d7301d4a83bf5e80a2afda17f4ce0e0ea3b2
Bryan <elton.flores.a@uni.pe>
2022-05-11 17:54:46 -0500

    adding news sort algorithms

--- Sorting/Sort.hpp
+++ Sorting/Sort.hpp
@@ -1,3 +1,4 @@
+#include <cmath>
 using namespace std;
 class Sort {
 public:
@@ -96,5 +97,100 @@ public:
       quickSort(array, inicio, j);
     if (i < fin)
       quickSort(array, i, fin);
+  }
+
+  void mezclaLista(int vector[], int izq, int cen, int der) {
+    int *temporal = new int[der + 1];
+    int i, j, k;
+    i = k = izq;
+    j = cen + 1;
+    while (i <= cen && j <= der) {
+      if (vector[i] < vector[j]) {
+        temporal[k] = vector[i];
+        i++;
+        k++;
+      } else {
+        temporal[k] = vector[j];
+        j++;
+        k++;
+      }
+    }
+    while (i <= cen)
+      temporal[k++] = vector[i++];
+    while (j <= der)
+      temporal[k++] = vector[j++];
+    // Copiando el temporal al vector
+    for (int k = izq; k <= der; k++)
+      vector[k] = temporal[k];
+  }
+
+  void mergeSort(int vector[], int izq, int der) {
+    int cen;
+    if (izq < der) {
+      cen = (izq + der) / 2;
+      mergeSort(vector, izq, cen);
+      mergeSort(vector, cen + 1, der);
+      mezclaLista(vector, izq, cen, der);
+    } else
+      return;
+  }
+
+  void heapSort(int vector[], int n) {
+    // Posición del último padre i = (n-1)/2
+    // Posición del hijo izquierdo = 2i+1
+    // Posición del hijo derecho = 2i+2
+    int i, izq, der, hi, hd;
+    for (n = n; n > 1; n--) {
+      i = (n - 1) / 2;
+      while (i >= 0) {
+        hi = 2 * i + 1;
+        hd = 2 * i + 2;
+        if (hd < n) {
+          if (vector[hd] > vector[i] && vector[hd] >= vector[hi])
+            swap(vector[hd], vector[i]);
+          else if (vector[hi] > vector[i] && vector[hi] >= vector[hd])
+            swap(vector[hi], vector[i]);
+        } else if (hi < n)
+          if (vector[hi] > vector[i])
+            swap(vector[hi], vector[i]);
+        i--;
+      }
+      swap(vector[0], vector[n - 1]);
+    }
+  }
+
+  void radixSort(int vector[], int n) {
+    int i, max, digMax = 0, p = 0, dig, j, k;
+    int c[10]{};                  // Vector que cuenta las columnas en la matriz
+    int **matriz = new int *[10]; // Crea la matriz
+    for (i = 0; i < 10; i++)
+      matriz[i] = new int[n]{};
+    // Hallando el maximo
+    max = vector[0];
+    for (i = 1; i < n; i++)
+      if (vector[i] > max)
+        max = vector[i];
+    // Hallando la cantidad de digitos del maximo
+    while (max > 0) {
+      max = max / 10;
+      digMax++;
+    }
+    // Algoritmo Heap
+    while (p < digMax) {
+      // Moviendo los numeros del vector a la matriz
+      for (i = 0; i < n; i++) {
+        dig = vector[i] / (int)pow(10, p) % 10;
+        matriz[dig][c[dig]] = vector[i];
+        c[dig]++;
+      }
+      // Subiendo la matriz al vector
+      k = 0;
+      for (i = 0; i < 10; i++) {
+        for (j = 0; j < c[i]; j++)
+          vector[k++] = matriz[i][j];
+        c[i] = 0;
+      }
+      p++;
+    }
   }
 };
