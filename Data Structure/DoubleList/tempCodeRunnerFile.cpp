9b9d092bc380e44666b56ca122a5f5f912f867f3
Bryan <elton.flores.a@gmail.com>
2022-06-08 15:09:40 -0500

    modified file structure

--- Data Structure/DoubleList/tempCodeRunnerFile.cpp
+++
@@ -1,2 +0,0 @@
-rintList();
-    retu
---
+++ Data Structure/Stack/main.cpp
@@ -0,0 +1,15 @@
+#include "StackWithVectors/Stack.hpp"
+#include <iostream>
+
+int main(){
+
+    Stack stack(5);
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+    stack.push(4);
+    stack.push(5);
+    stack.print();
+
+
+}
